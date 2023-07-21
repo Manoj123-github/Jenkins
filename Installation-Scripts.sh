437789b6af4718be197f10047bcb3c177c07e91b
Manoj123-github <manoj.h.yadav1002@gmail.com>
2023-07-22 00:38:27 +0530

    jenkins installation

---
+++ Installation-Scripts.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+# Install Java to run jenkins
+sudo apt update
+sudo apt install openjdk-17-jre
+
+
+# install Jenkins
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
